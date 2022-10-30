a7b680010e6e21e324a4c028c3dae0ec414a6188
Matias Pan <matipan@hey.com>
2022-10-30 11:50:11 -0300

    Pin nix

--- shell.nix
+++ shell.nix
@@ -1,11 +1,9 @@
-let
-  getpkgs = import <nixpkgs>;
-  pkgs = getpkgs {};
-in
-  pkgs.mkShell {
-    buildInputs = [
-      pkgs.curl
-      pkgs.git
-      pkgs.nodejs-18_x
-    ];
-  }  
+{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/040c6d8374d090f46ab0e99f1f7c27a4529ecffd.tar.gz") {} }:
+
+pkgs.mkShell {
+  buildInputs = [
+    pkgs.curl
+    pkgs.git
+    pkgs.nodejs-18_x
+  ];
+}
